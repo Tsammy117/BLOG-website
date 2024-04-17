58f07a0d3d35c3b9e6754d4c403c776a7663fa64
Sammed Tare <127128991+Tsammy117@users.noreply.github.com>
2024-04-17 22:28:34 +0530

    connection to data base
    mongoDB used

---
+++ client/src/components/account/login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Box,TextField} from '@mui/material'
+const Login = () => {
+  return (
+    <Box>
+        <h1>hello from login</h1>
+        <TextField />
+    </Box>
+  )
+}
+
+export default Login
--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import connectDB from './database/db.js';
 const app=express();
 const PORT=8000;
 app.listen(PORT,()=>console.log(`server is running at ${PORT}`));
+connectDB();
